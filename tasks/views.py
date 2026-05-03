cfebc6315457d801f8fdd3b8c4b70860afb904d4
rahaf ali <rahaf1423rahaf@gmail.com>
2026-05-03 19:36:40 +0300

    add tsaks button

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task 
+from .forms import TaskForm
 
 def task_list(request):
-    all_tasks = Task.objects.all()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()    
     
-    return render(request, 'tasks/list.html', {'tasks': all_tasks})
+    all_tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': all_tasks, 'form': form})
